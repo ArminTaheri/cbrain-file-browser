71cd0acf7dae9f734f46486acc64a122dfd9c298
ArminTaheri <armin.ytaheri@gmail.com>
2018-01-15 18:39:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import FileManager from './lib/file-manager/FileManager';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <FileManager />;
+  }
+}
+
+export default App;
---
+++ src/lib/file-manager/FileManager.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { componentFromEventStream } from 'recompose';
+
+export default () => {
+  return (
+    <div className='file-manager'>
+      Files baby.
+    </div>
+  );
+}
